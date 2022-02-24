febc56e193c53156bffb0cea0e4378f1402f53eb
rardooba <rardooba@gmail.com>
2022-02-24 12:25:01 +0100

    Add <details> custom  version >> Dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,25 +1,37 @@
-import React, { useState } from 'react';
+
+//! Sans la balise <DETAILS> en React
+//import React, { useState } from 'react';
 import "./index.css";
 
 const Dropdown = ({ title, children }) => {
 
-    const [isOpen, setIsOpen] = useState(false)
+    //! Sans la balise <DETAILS> en React
+    //const [isOpen, setIsOpen] = useState(false)
 
 
     return (
-        <div className="dropdown">
-        <h3 onClick={() => setIsOpen(isOpen ? false : true)}>
-          {title}
-          <span
-            className={isOpen ? "fas fa-chevron-up" : "fas fa-chevron-down"}
-          ></span>
-        </h3>
-        <div className={`${isOpen ? "drop-open" : "drop-close"}`}>
+        <details className="dropdown">
+          <summary>{title}</summary>
           {children}
-        </div>
-        
-      </div>
+        </details>
     );
+
+
+    //! Sans la balise <DETAILS> en React
+    // return (
+    //     <div className="dropdown">
+    //     <h3 onClick={() => setIsOpen(isOpen ? false : true)}>
+    //       {title}
+    //       <span
+    //         className={isOpen ? "fas fa-chevron-up" : "fas fa-chevron-down"}
+    //       ></span>
+    //     </h3>
+    //     <div className={`${isOpen ? "drop-open" : "drop-close"}`}>
+    //       {children}
+    //     </div>
+        
+    //   </div>
+    // );
 };
 
 export default Dropdown;
